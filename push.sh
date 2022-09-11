5429ff4b4bcd1a18f11fcd73e543d4e29ef3867d
dynamo58 <sailorfirex@seznam.cz>
2022-09-12 00:23:09 +0200

    (fix): fixed order of commands in `push.sh` script

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ echo "$_message"
 sed -i '10s/.*/	return db_client.database("data").collection<T>(label)/' src/db/db.ts
 
 git add .
-git push origin main
 git commit -m "$_message"
+git push origin main
 
 sed -i '10s/.*/	return db_client.database("testing").collection<T>(label)/' src/db/db.ts
